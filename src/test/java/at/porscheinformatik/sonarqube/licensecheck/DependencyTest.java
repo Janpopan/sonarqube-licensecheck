e46c845f492525e66ce7a175064f55c5f6991e9f
Jan Hill <janpopan@gmx.net>
2021-05-28 22:49:49 +0200

    improve test

--- src/test/java/at/porscheinformatik/sonarqube/licensecheck/DependencyTest.java
+++ src/test/java/at/porscheinformatik/sonarqube/licensecheck/DependencyTest.java
@@ -3,12 +3,12 @@ package at.porscheinformatik.sonarqube.licensecheck;
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItems;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 import java.util.List;
 
+import at.porscheinformatik.sonarqube.licensecheck.mavendependency.MavenDependency;
 import org.junit.Test;
 
 public class DependencyTest {
@@ -21,83 +21,83 @@ public class DependencyTest {
 
     @Test
     public void testConstructor() {
-        // Arrange and Act
         Dependency actualDependency = new Dependency("Name", "1.0.2", "License");
         actualDependency.setStatus(Dependency.Status.Allowed);
 
-        // Assert
         assertEquals(Dependency.Status.Allowed, actualDependency.getStatus());
     }
 
     @Test
     public void testGetName() {
-        // Arrange, Act and Assert
         assertEquals("Name", (new Dependency("Name", "1.0.2", "License")).getName());
     }
 
     @Test
     public void testGetVersion() {
-        // Arrange, Act and Assert
         assertEquals("1.0.2", (new Dependency("Name", "1.0.2", "License")).getVersion());
     }
 
     @Test
     public void testGetLicense() {
-        // Arrange, Act and Assert
         assertEquals("License", (new Dependency("Name", "1.0.2", "License")).getLicense());
     }
 
     @Test
     public void testGetPomPath() {
-        // Arrange, Act and Assert
         assertNull((new Dependency("Name", "1.0.2", "License")).getPomPath());
     }
 
     @Test
     public void testHashCode() {
-        // Arrange, Act and Assert
         assertEquals(1309258900, (new Dependency("Name", "1.0.2", "License")).hashCode());
     }
 
     @Test
     public void testToString() {
-        // Arrange, Act and Assert
         assertEquals("{name:Name, version:1.0.2, license:License}",
             (new Dependency("Name", "1.0.2", "License")).toString());
     }
 
     @Test
     public void testCompareTo() {
-        // Arrange
         Dependency dependency = new Dependency("Name", "1.0.2", "License");
 
-        // Act and Assert
         assertEquals(0, dependency.compareTo(new Dependency("Name", "1.0.2", "License")));
     }
 
     @Test
     public void testCompareTo2() {
-        // Arrange
         Dependency dependency = new Dependency(null, "1.0.2", "License");
 
-        // Act and Assert
         assertEquals(-1, dependency.compareTo(new Dependency("Name", "1.0.2", "License")));
     }
 
     @Test
     public void testCompareTo3() {
-        // Arrange
         Dependency dependency = new Dependency("Name", "1.0.2", "License");
 
-        // Act and Assert
         assertEquals(1, dependency.compareTo(new Dependency(null, "1.0.2", "License")));
     }
 
     @Test
     public void testCompareTo4() {
-        // Arrange, Act and Assert
         assertEquals(1, (new Dependency("Name", "1.0.2", "License")).compareTo(null));
     }
+
+    @Test
+    public void testEquals() {
+        Dependency d1=new Dependency("Name", "1.0.2", "License");
+        Dependency d2=new Dependency("Name", "1.0.2", "License");
+        Dependency d3=new Dependency("Name1", "1.0.2", "License1");
+        Object o = new Object();
+
+        assertTrue(d1.equals(d2));
+        assertTrue(d1.equals(d1));
+        assertFalse(d1.equals(d3));
+        assertFalse(d1.equals(null));
+        assertFalse(d1.equals(o));
+    }
+
 
     @Test
     public void createString() {
